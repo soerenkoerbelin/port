d221a6ca1fc57a1da127f049517ccb0c3c3f379f
soeren.koerbelin <soeren.koerbelin@gmail.com>
2020-05-02 12:43:16 +0200

    Updates

---
+++ precache-manifest.8493d49c3ce367c1b5973778efeee35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f4e21c4ded659e3366e7ea6b4b9366",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "e5baba99a03c3c23afae",
+    "url": "/port/static/css/main.7a8e59bc.chunk.css"
+  },
+  {
+    "revision": "4fd1aaf78a1f7c96ae07",
+    "url": "/port/static/js/2.5eaf0f0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/port/static/js/2.5eaf0f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5baba99a03c3c23afae",
+    "url": "/port/static/js/main.db4a8ac0.chunk.js"
+  },
+  {
+    "revision": "782dff2bfa7256261e01",
+    "url": "/port/static/js/runtime-main.ab8b9762.js"
+  }
+]);
